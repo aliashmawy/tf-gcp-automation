6f2af8430cf692ea063be799dcf9132fcc352780
Ali Hussein <aliashmawy595@gmail.com>
2025-09-28 17:38:03 +0300

    add deletion policy

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -2,6 +2,7 @@ resource "google_project" "my_project" {
   name       = var.project_name
   project_id = var.project_id
   billing_account = data.google_billing_account.acct.id
+  deletion_policy = "DELETE"
 }
 data "google_billing_account" "acct" {
   display_name = "My Billing Account"
